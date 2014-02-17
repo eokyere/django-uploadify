a55c8a0ac34106ff05286eed26ee2219e43fb4a5
Donald Okie <dok3@24x7.cl>
2014-02-16 21:48:01 -0300

    fix

--- uploadify/urls.py
+++ uploadify/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import *
 
+import views
+
 urlpatterns = patterns('',
+    url(r'^uploaded_view/$', views.uploaded_view),
+    url(r'^upload/', views.upload),
 )
 
--- uploadify/views.py
+++ uploadify/views.py
@@ -1,13 +1,26 @@
 import os
 import time
 import uuid
+import json
 
 from django.conf import settings
+from django.http import HttpResponse
 
-def upload_handler(FILES):
-    file = FILES['Filedata']
+from sorl.thumbnail import get_thumbnail
 
-    DEST_DIR = settings.UPLOADIFY_UPLOAD_PATH+"/"+time.strftime('%Y/%m/%d')
+def upload(request):
+    if request.session.has_key('form_img'):
+        try:
+            removeMe = settings.MEDIA_ROOT+"/"+request.session['form_img']
+            os.unlink(removeMe)
+        except Exception, e:
+            print "Exception:",str(e)
+            pass
+
+    file = request.FILES['Filedata']
+
+    RELATIVE_DEST_DIR = settings.UPLOADIFY_UPLOAD_PATH+"/"+time.strftime('%Y/%m/%d')
+    DEST_DIR = settings.MEDIA_ROOT+"/"+RELATIVE_DEST_DIR
 
     try:
         os.makedirs(DEST_DIR, 0755)
@@ -17,8 +30,17 @@ def upload_handler(FILES):
 
     ext = file.name.split('.')[-1]
     fileName = "%s.%s" % (uuid.uuid4(), ext)
-    destination = open(DEST_DIR + "/" + fileName, 'wb+')
+    SessionDestination = RELATIVE_DEST_DIR + "/" + fileName
+    Destination = DEST_DIR + "/" + fileName
+
+    destination = open(Destination, 'wb+')
     for chunk in file.chunks():
         destination.write(chunk)
 
-    return (file.name, fileName, DEST_DIR)
+    request.session['form_img'] = SessionDestination
+    #return (file.name, fileName, DEST_DIR)
+    return HttpResponse(json.dumps({'status':'uploaded', 'file':SessionDestination}), content_type="application/json")
+
+def uploaded_view(request):
+    img = get_thumbnail(request.session['form_img'], '200x200', crop='center', format="PNG")
+    return HttpResponse('<img alt="Image" border="0" src="'+settings.MEDIA_URL+img.name+'" width="'+str(img.width)+'" height="'+str(img.height)+'">')
